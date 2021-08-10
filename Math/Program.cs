ba9d749d334b4fc15757ed3394e61c48d8e041de
YunusEmre <yunus@YUNUSEMRE>
2021-08-10 20:47:45 +0300

    10.08.21

---
+++ Math/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5,4);
+        }
+    }
+}
