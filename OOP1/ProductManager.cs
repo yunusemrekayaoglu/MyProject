6ff1b70a4c7fcc7671294829c3bcaa475f878006
YunusEmre <yunus@YUNUSEMRE>
2021-08-11 15:27:05 +0300

    11.08.21

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " has been successfully added!");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " has been successfully updated!");
+        }
+        public void Delete(Product product)
+        {
+            Console.WriteLine(product.ProductName + " has been succesfully removed!");
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product()
+            {
+                Id              = 1,
+                CategoryId      = 1,
+                ProductName     = "Masa",
+                UnitPrice       = 300,
+                UnitsInStock    = 50
+            };
+            Product product2 = new Product()
+            {
+                Id = 2,
+                CategoryId = 2,
+                ProductName = "Sandalye",
+                UnitPrice = 2300,
+                UnitsInStock = 150
+            };
+            Product product3 = new Product()
+            {
+                Id = 3,
+                CategoryId = 3,
+                ProductName = "Laptop",
+                UnitPrice = 3100,
+                UnitsInStock = 503
+            };
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            ProductManager productManager = new ProductManager();
+
+            foreach (var product in products)
+            {
+                productManager.Add(product);
+            }
+        }
+    }
+}
