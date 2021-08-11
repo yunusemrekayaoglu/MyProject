8d60e6afe41241c99a8ed065a75f8faa2fe4f010
YunusEmre <yunus@YUNUSEMRE>
2021-08-11 16:00:41 +0300

    11.08.21

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[]
+            {
+                "Yunus",
+                "Emre",
+                "Kayaoğlu"
+            };
+
+            List<string> names2= new List<string>
+            {
+                "Yunus",
+                "Emre",
+                "Kayaoğlu"
+            };
+
+            foreach (var name in names2)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Yunus");
+
+            Console.WriteLine(names.Length);
+
+            names.Add("Emre");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
