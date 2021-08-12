111e2341ae2b42f2d7781c9db1e85709b22fa05b
YunusEmre <yunus@YUNUSEMRE>
2021-08-12 18:29:39 +0300

    12.08.21

---
+++ GenericsIntroAgainv2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericsIntroAgainv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Yunus");
+            Console.WriteLine(names.Length);
+            names.Add("Emre");
+            Console.WriteLine(names.Length);
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
