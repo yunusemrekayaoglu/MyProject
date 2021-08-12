43921530e8dfc21c0d12043370282edc909ac914
YunusEmre <yunus@YUNUSEMRE>
2021-08-12 19:33:28 +0300

    12.08.21

---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { FirstName = "Yunus" };
+            Person person2 = new Person();
+
+            person2 = person1;
+            person1.FirstName = "Emre";
+
+            //Console.WriteLine(person2.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Kaya";
+            Employee employee = new Employee();
+            employee.FirstName = "ZA";
+
+            Person person3 = customer;
+            //Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(employee);
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+    class Employee : Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
