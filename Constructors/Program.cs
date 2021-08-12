64d74993724f5862be43311fd9e19604c440fc27
YunusEmre <yunus@YUNUSEMRE>
2021-08-12 20:02:21 +0300

    12.08.21

---
+++ Constructors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Yunus",
+                LastName = "Kayaoğlu",
+                City = "Aydın"
+            };
+
+            Customer customer2 = new Customer(1, "Yunus", "Emre", "Aydın");
+        }
+        
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = FirstName;
+            LastName = lastName;
+            City = city;
+            Console.WriteLine("Yapıcı blok çalıştı!");
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cities = new List<string>();
+
+            cities.Add("Aydınsz");
+            cities.Add("Aydınsz");
+            cities.Add("Aydınsz");
+            cities.Add("Aydınsz");
+            cities.Add("Aydınsz");
+
+            Console.WriteLine(cities.Count);
+
+            MyList<string> cities2 = new MyList<string>();
+            cities2.Add("Aydın");
+            cities2.Add("Aydın");
+            cities2.Add("Aydın");
+            cities2.Add("Aydın");
+            cities2.Add("Aydın");
+            cities2.Add("Aydın");
+            Console.WriteLine(cities2.Count);
+
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        private int _count;
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
