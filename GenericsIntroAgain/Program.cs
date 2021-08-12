6d1180432dbe57140db7d2434b45f60a7a4ba9d5
YunusEmre <yunus@YUNUSEMRE>
2021-08-12 18:23:01 +0300

    12.08.21

---
+++ GenericsIntroAgain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericsIntroAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("asd");
+
+            Console.WriteLine(names.Length);
+            names.Add("asfg");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
