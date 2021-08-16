ecca9e24524daa5e52bb76e54caf97af5f6cd75d
YunusEmre <yunus@YUNUSEMRE>
2021-08-16 13:35:27 +0300

    16.08.21

---
+++ Interfaces2/Program.cs
@@ -0,0 +1,53 @@
+﻿ProjectManager personManager = new ProjectManager();
+personManager.Add(new InternManager());
+
+
+interface IPersonManager
+{
+    void Add();
+    void Update();
+}
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("müşteri eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("müşteri güncellendi");
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("personel eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("personel güncellendi");
+    }
+}
+class InternManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("stajyer eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("stajyer güncellendi");
+    }
+}
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+}
